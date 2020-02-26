bedafb84ab4141bfa2da2302c84822a3c72ee705
ChocoTaco <boldfilter@zoho.com>
2020-02-26 18:39:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,9 +11,8 @@ $(document).ready(function () {
     }
 
     function updateView(data){
-        var template = "<div><strong>"+data.info_hostname+"</strong></div>";
+        var template = "<div><strong>"+data.info_hostname+"</strong></div>" + " <div>["+data.num_players+" / "+ data.info_flags.max_players +"]</div>";
         var template = template + "<div>"+ data.info_map+" — "+data.info_maptype+"</div>";
-        var template = template + "<div>"+data.num_players+" / "+ data.info_flags.max_players +"</div>";
         $serverStatContainer.html('<div bgcolor="" style="padding: 0px 0px 0px 0px;text-shadow: -1px 1px 1px #111111;line-height: 16px;font-size:14px; color:#0fcfb9;text-align: right; font-family: sans-serif;mso-height-rule: exactly">'+ template + '</div>');
     }
 
