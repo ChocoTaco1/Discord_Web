2789561803944453d3848820f549554cefd6e20f
ChocoTaco <boldfilter@zoho.com>
2020-02-27 13:48:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,8 +37,8 @@ $(document).ready(function () {
 		var template = template + "<div>["+data.num_players+" / "+ data.info_flags.max_players +"]</div>";
 		var template = template + "<div>"+data.info_map+" - "+data.info_maptype+"</div>";
 		var template = template + "<div></div>";
-		var template = template + "<div>"+data.info_players.map(function(players){
-			return data.info_players.name
+		var template = template + "<div>Observers: "+data.info_players.map(function(players){
+			return data.info_players.0.name
 		}).join('')+"</div>";
         $FULLserverStatContainer.html('<div bgcolor="" style="padding: 0px 0px 0px 0px;text-shadow: -1px 1px 1px #111111;line-height: 20px;color:#cccccc;font-size:14px;text-align: right; font-family: sans-serif;">'+ template + '</div>');
     }
