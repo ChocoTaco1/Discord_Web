fd33cf7edbf490a646d2734ffbfaed2ff55c8639
ChocoTaco <boldfilter@zoho.com>
2020-02-26 19:31:29 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
     }
 
     function updateView(data){
-        var template = "<div><strong>"+data.info_hostname+"</strong> ["+data.num_players+"&nbsp;/&nbsp;"+ data.info_flags.max_players +"]</div>";
+        var template = "<div style=color:#cccccc;"><strong>"+data.info_hostname+"</strong> ["+data.num_players+"&nbsp;/&nbsp;"+ data.info_flags.max_players +"]</div>";
         var template = template + "<div>"+ data.info_map+" - "+data.info_maptype+"</div>";
-        $serverStatContainer.html('<div bgcolor="" style="padding: 0px 0px 0px 0px;text-shadow: -1px 1px 1px #111111;line-height: 18px;font-size:14px; color:#07bec0;text-align: right; font-family: sans-serif;mso-height-rule: exactly">'+ template + '</div>');
+        $serverStatContainer.html('<div bgcolor="" style="padding: 0px 0px 0px 0px;text-shadow: -1px 1px 1px #111111;line-height: 18px;font-size:14px;text-align: right; font-family: sans-serif;mso-height-rule: exactly">'+ template + '</div>');
     }
 
     getServerData(discordServerIP)
