9a0d0e4c398c42959f53bbed82b7fc9c6377b7a2
ChocoTaco <boldfilter@zoho.com>
2020-02-29 16:27:40 -0500

    Server html update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var $serverStatContainer = $('#serverStatContainer');
+    var $FULLserverStatContainer = $('#FULLserverStatContainer');
     var discordServerIP = "67.222.138.12:28000";
     function getServerData(ip) {
         $.getJSON('https://www.tribesnext.com/json', function (tnMasterServerData) {
@@ -7,6 +8,7 @@ $(document).ready(function () {
                 return server.s_ipa === ip;
             });
             updateView(result)
+            FULLupdateView(result)
         });
     }
 
@@ -18,27 +20,16 @@ $(document).ready(function () {
 
     getServerData(discordServerIP)
     setInterval(function(){ getServerData(discordServerIP) }, 25000);   // check every 25 seconds 
-});
-
-$(document).ready(function () {
-    var $FULLserverStatContainer = $('#FULLserverStatContainer');
-    var discordServerIP = "91.55.60.117:28000";
-    function getServerData(ip) {
-        $.getJSON('https://www.tribesnext.com/json', function (tnMasterServerData) {
-            var result = tnMasterServerData.find(function (server) {
-                return server.s_ipa === ip;
-            });
-			FULLupdateView(result)
-        });
-    }
 	
 	function FULLupdateView(data){
         var template = "<div><strong>DAMN,</strong></div>"; 
-		var template = template + "<div>there's "+data.num_players+"</div>";
-		var template = template + "<div>players on</div>";
-		var template = template + "<div>"+data.info_hostname+"</div>";
+		var template = template + "<div>there's <strong>"+data.num_players+"</strong></div>";
+        var data_players = data.num_players;
+        if(data_players==1){num_players = "player";}else{num_players = "players";}
+		var template = template + "<div>"+num_players+" on</div>";
+		var template = template + "<div><strong>"+data.info_hostname+"</strong></div>";
         var template = template + "<div>right now</div>";
-        $FULLserverStatContainer.html('<div bgcolor="" style="padding: 0px 0px 0px 0px;text-shadow: -1px 1px 1px #111111;line-height: 40px;color:#cccccc;font-size:40px;text-align: right; font-family: sans-serif;">'+ template + '</div>');
+        $FULLserverStatContainer.html('<div bgcolor="" style="padding: 10px 0px 20px 0px;text-shadow: -1px 1px 1px #111111;line-height: 120px;color:#cccccc;font-size:120px;text-align: center; font-family: sans-serif;">'+ template + '</div>');
     }
 
     getServerData(discordServerIP)
