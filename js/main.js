102389d26c7c1fca8a55869268579b790429abc8
ChocoTaco <boldfilter@zoho.com>
2020-02-29 17:35:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,17 +14,17 @@ $(document).ready(function () {
 
     function updateView(data){
         var template = "<div><strong>"+data.info_hostname+"</strong> ["+data.num_players+" / "+ data.info_flags.max_players +"]</div>";
-		var template = template + "<div>"+ data.info_map+" - "+data.info_maptype+"</div>";
+            template = template + "<div>"+ data.info_map+" - "+data.info_maptype+"</div>";
         $serverStatContainer.html('<div bgcolor="" style="padding: 0px 0px 0px 0px;text-shadow: -1px 1px 1px #111111;line-height: 20px;color:#cccccc;font-size:14px;text-align: right; font-family: sans-serif;"><a href="server.html" style="text-decoration: none">'+ template + '</a></div>');
     }
 	
 	function FULLupdateView(data){
-        var template = "<div><strong>DAMN,</strong></div>"; 
-		var template = template + "<div>there's <strong>"+data.num_players+"</strong></div>";
         var data_players = data.num_players;
-		var template = template + "<div>player" + (data_players != 1 ? "s":"") + " on</div>";
-		var template = template + "<div><strong>"+data.info_hostname+"</strong></div>";
-        var template = template + "<div>right now</div>";
+        var template = "<div><strong>DAMN,</strong></div>"; 
+            template = template + "<div>there's <strong>"+data.num_players+"</strong></div>";
+            template = template + "<div>player" + (data_players != 1 ? "s":"") + " on</div>";
+            template = template + "<div><strong>"+data.info_hostname+"</strong></div>";
+            template = template + "<div>right now</div>";
         $FULLserverStatContainer.html('<div bgcolor="" style="padding: 10px 0px 20px 0px;text-shadow: -3px 2px 2px #111111;line-height: 120px;color:#cccccc;font-size:120px;text-align: center; font-family: sans-serif;">'+ template + '</div>');
     }
 
