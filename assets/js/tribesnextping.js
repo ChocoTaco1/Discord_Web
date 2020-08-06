a7b032cb6b11e650ab446cf43bab93593f0af892
ChocoTaco1 <boldfilter@zoho.com>
2020-08-05 21:24:57 -0400

    Update

--- assets/js/tribesnextping.js
+++ assets/js/tribesnextping.js
@@ -13,20 +13,20 @@ $(document).ready(function () {
     }
 
     function updateView(data){
-        var template = "<div>"+data.info_hostname+"&nbsp;&nbsp;&nbsp;&nbsp; P#: "+data.num_players+"/"+ data.info_flags.max_players+"</div>";
-		template = "<div>"+template+data.info_map+"&nbsp; / &nbsp;"+data.info_maptype+"</div>";
-        $serverStatContainer.html('<div bgcolor="" style="font-size:16px;line-height: 17px;"><a href="server.html" style="text-align: center; color:#545c61;">'+template+'</a></div>');
+        var template = "<div>" + data.info_hostname + "&nbsp;&nbsp;&nbsp;&nbsp; P#: " +data.num_players + "/" + data.info_flags.max_players + "</div>";
+		template = "<div>" + template + data.info_map + "&nbsp; / &nbsp;" + data.info_maptype + "</div>";
+        $serverStatContainer.html('<div bgcolor="" style="font-size:16px;line-height: 17px;"><a href="server.html" style="text-align: center; color:#545c61;">'+ template + '</a></div>');
     }
 	
 	function FULLupdateView(data){
         var data_players = data.num_players;
         var template = "<div>SHAZBOT,</div>"; 
-            template = template + "<div>there's <strong style=\"color:#0a9ba8;\">"+data.num_players+"</strong> player"+(data_players != 1 ? "s":"")+" on</div>";
-            template = template + "<div>"+data.info_hostname+"</div>";
+            template = template + "<div>there's <strong style=\"color:#0a9ba8;\">" + data.num_players + "</strong> player" + (data_players != 1 ? "s":"") + " on</div>";
+            template = template + "<div>"+ data.info_hostname + "</div>";
             template = template + "<div>right now playing</div>";
-			template = template + "<div>"+data.info_map+"</div>";
-			template = template + "<div>"+data.info_maptype+"</div>";
-        $FULLserverStatContainer.html('<div bgcolor="" style="line-height: 100px;font-size:85px;text-align: center;">'+template+'</div>');
+			template = template + "<div>" + data.info_map + "</div>";
+			template = template + "<div>" + data.info_maptype + "</div>";
+        $FULLserverStatContainer.html('<div bgcolor="" style="line-height: 100px;font-size:85px;text-align: center;">' + template + '</div>');
     }
 
     getServerData(discordServerIP)
