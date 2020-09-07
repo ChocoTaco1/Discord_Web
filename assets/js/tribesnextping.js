82f46a251f093dc71616de1808ed8c15d1e1ec61
ChocoTaco1 <boldfilter@zoho.com>
2020-09-07 15:42:50 -0400

    Added Pop Details

--- assets/js/tribesnextping.js
+++ assets/js/tribesnextping.js
@@ -9,11 +9,9 @@ $(document).ready
 		function getServerData(ip) 
 		{
 			$.getJSON('https://www.tribesnext.com/list.json', 
-				function(tnMasterServerData)
-				{
+				function(tnMasterServerData){
 					var result = tnMasterServerData.find(
-						function(server) 
-						{
+						function(server) {
 							return server.s_ipa === ip;
 						}
 					);
@@ -40,15 +38,65 @@ $(document).ready
 			template += '<div>right now playing</div>';
 			template += '<div>' + data.info_map + '</div>';
 			template += '<div>' + data.info_maptype + '</div>';
+
+			
+			function objectLength(obj) {
+				var result = 0;
+				for(var prop in obj) {
+					if (obj.hasOwnProperty(prop)) {
+					// or Object.prototype.hasOwnProperty.call(obj, prop)
+					result++;
+					}
+				}
+				if(obj != team0){
+					result -= 3; 
+					if(result < 0) result = 0; //Minus team name, score, and ???
+				}
+
+				return result;
+			}
+			
+			if(data_players > 0)
+			{
+				var teams = data.info_players;
+
+				var team0 = teams[0];
+				var team1 = teams[1];
+				var team2 = teams[2];
+				
+				var team0cnt = objectLength(team0);
+				var team1cnt = objectLength(team1);
+				var team2cnt = objectLength(team2);
+
+				if(team1cnt > 0){
+					template += '<br>' + team1.name + "&nbsp;&nbsp;&nbsp;&nbsp;" + team1.score + '<br>';
+					for (i = 0; i < team1cnt; i++) {
+						template += team1[i].name + "&nbsp;&nbsp;&nbsp;&nbsp;" + team1[i].score + "<br>";
+					}
+				}
+				
+				if(team2cnt > 0){
+					template += '<br>' + team2.name + "&nbsp;&nbsp;&nbsp;&nbsp;" + team2.score + '<br>';
+					for (i = 0; i < team2cnt; i++) {
+						template += team2[i].name + "&nbsp;&nbsp;&nbsp;&nbsp;" + team2[i].score + "<br>";
+					}
+				}
+				
+				if(team0cnt > 0){
+					template += '<br>' + "Observers" + '<br>';
+					for (i = 0; i < team0cnt; i++) {
+						template += team0[i].name + "&nbsp;&nbsp;&nbsp;&nbsp;" + team0[i].score + "<br>";
+					}
+				}
+				
+			}
+			
 			$serverPopContainerPage.html('<div bgcolor="" style="text-align: center;">' + template + '</div>');
 		}
 
 		getServerData(discordServerIP);
 		setInterval(
-			function() 
-			{
-				getServerData(discordServerIP);
-			}, 25000 // check every 25 seconds
+			function() { getServerData(discordServerIP); }, 25000 // check every 25 seconds
 		);
 	}
 );
