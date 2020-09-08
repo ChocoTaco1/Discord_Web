9fe58d98e271ced7fecf45f90d37c2054b997f17
ChocoTaco1 <boldfilter@zoho.com>
2020-09-07 21:23:49 -0400

    Update tribesnextping.js

--- assets/js/tribesnextping.js
+++ assets/js/tribesnextping.js
@@ -95,40 +95,38 @@ $(document).ready
 					var team2cnt = objectLength(team2);
 					
 					//Team 1 and Team 2
-					if(team1cnt > 0 || team2cnt > 0){
-						template += '<div class="row">';
-						template += '<div class="col-4">';
-						template += '<div class="column" style="text-decoration: underline">' + team1.name +  '</div>';
-						for (i = 0; i < team1cnt; i++){
-							if(team1[i].name === ""){
-							team0cnt++;	
-							continue;
-							}
-							template += '<div class="column">' + team1[i].name  + '</div>';
+					template += '<div class="row">';
+					template += '<div class="col-4" style="min-width:200px">';
+					template += '<div class="column" style="text-decoration: underline">' + team1.name +  '</div>';
+					for (i = 0; i < team1cnt; i++){
+						if(team1[i].name === ""){
+						team0cnt++;	
+						continue;
 						}
-						template += '</div>';
-						template += '<div class="col-4">';
-						template += '<div class="column" style="text-decoration: underline">' + team2.name + '</div>';
-						for (i = 0; i < team2cnt; i++){
-							if(team2[i].name === ""){
-							team0cnt++;	
-							continue;
-							}
-							template += '<div class="column">' + team2[i].name+ '</div>';
-						}
-						//Observers
-						template += '</div>';
-						template += '<div class="col-4">';
-						template += '<div class="column" style="text-decoration: underline">' + "Observers" + '</div>';
-						for (i = 0; i < team0cnt; i++){
-							if(team0[i].name === ""){
-								team0cnt++;	
-								continue;
-							}
-							template += '<div class="column">' + team0[i].name + '</div>';
-						}
-						template += '</div>';
+						template += '<div class="column">' + team1[i].name  + '</div>';
 					}
+					template += '</div>';
+					template += '<div class="col-4" style="min-width:200px">';
+					template += '<div class="column" style="text-decoration: underline">' + team2.name + '</div>';
+					for (i = 0; i < team2cnt; i++){
+						if(team2[i].name === ""){
+						team0cnt++;	
+						continue;
+						}
+						template += '<div class="column">' + team2[i].name+ '</div>';
+					}
+					//Observers
+					template += '</div>';
+					template += '<div class="col-4" style="min-width:200px">';
+					template += '<div class="column" style="text-decoration: underline">' + "Observers" + '</div>';
+					for (i = 0; i < team0cnt; i++){
+						if(team0[i].name === ""){
+							team0cnt++;	
+							continue;
+						}
+						template += '<div class="column">' + team0[i].name + '</div>';
+					}
+					template += '</div>';
 				}
 			}
 			$serverPopContainerList.html('<div style="font-size:45px;line-height: 55px;text-align: center;">' + template + '</div>');
