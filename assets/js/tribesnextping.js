e9e7189d7909ae6f555a8cf4914c2624597acb34
ChocoTaco1 <boldfilter@zoho.com>
2020-09-08 15:36:40 -0400

    Update tribesnextping.js

--- assets/js/tribesnextping.js
+++ assets/js/tribesnextping.js
@@ -25,34 +25,34 @@ $(document).ready
 			);
 		}
 
-		var div ='<div>';
+		var div = '<div>';
 		var div1 = '</div>';
 		
 		function updateView(data) 
 		{
-			var template = div + data.info_hostname + '&#160;&#160; P#: ' + data.num_players + '/' + data.info_flags.max_players + div1;
-			template = div + template + data.info_map + '&#160; / &#160;' + data.info_maptype + div1;
+			var template = `${div} ${data.info_hostname} &#160; &#160; P#: ${data.num_players} / ${data.info_flags.max_players} ${div1}
+                            ${div} ${template} ${data.info_map} &#160;/&#160; ${data.info_maptype} ${div1}`;
 			
-			$serverPopContainer.html('<div bgcolor="" style="font-size:16px;line-height: 17px;"><a href="server.html" style="text-align: center; color:#545c61;">' + template + '</a>' + div1);
+			$serverPopContainer.html(`<div bgcolor="" style="font-size:16px;line-height: 17px;"><a href="server.html" style="text-align: center; color:#545c61;"> ${template} </a> ${div1}`);
 		}
 
 		function updateViewPage(data) 
 		{
 			var players = data.num_players;
-			var template = div + 'SHAZBOT,' + div1;
-			template += div + 'there\'s <strong style="color:#0a9ba8;">' + data.num_players + '</strong> player' + (players != 1 ? 's' : '') + ' on' + div1;
-			template += div + data.info_hostname + div1;
-			template += div + 'right now playing' + div1;
-			template += div + data.info_map + div1;
-			template += div + data.info_maptype + div1;
+			var template = `${div} SHAZBOT, ${div1}
+							${div} there\'s <strong style="color:#0a9ba8;"> ${data.num_players} </strong> player${(players != 1 ? 's' : '')} on ${div1}
+							${div} ${data.info_hostname} ${div1}
+							${div} right now playing ${div1}
+							${div} ${data.info_map} ${div1}
+							${div} ${data.info_maptype} ${div1}`;
 
-			$serverPopContainerPage.html('<div bgcolor="" style="text-align: center;">' + template + '</div>');
+			$serverPopContainerPage.html(`<div bgcolor="" style="text-align: center;"> ${template} ${div1}`);
 		}
 		
 		function updateViewList(data) 
 		{
 			var players = data.num_players;
-			var template = '<br>';
+			var template = ``;
 
 			function objectLength(teamnum, obj){
 				var result = 0;
@@ -73,36 +73,37 @@ $(document).ready
 				var teamdata = data[teamnum];
 				var count = objectLength(teamnum, teamdata);
 				if(data.info_maptype != "LakRabbit")
-					template += div4 + teamdata.name + div1;
+					template = `${template} ${div4} ${teamdata.name} ${div1}`;
 				if(count > 0){
 					for (i = 0; i < count; i++){
 						if(teamdata[i].name === "")
 							continue;
-						template += divc + teamdata[i].name  + div1;
+						template = `${template} ${divc} ${teamdata[i].name} ${div1}`;
 					}
-					template += div1br;
+					template = `${template} ${div1br}`;
 				}
 				else
-					template += 'N/A' + div1br;
+					template = `${template} N/A ${div1br}`;
 			}
 			
 			if(players > 0){
+				template = `<br>`;
 				var data = data.info_players;
 				
 				//Formatting
-				var div4 = '<div class="col-4" style="min-width:250px;"><div class="column" style="text-decoration: underline;">';
-				var divc = '<div class="column">';
-				var div1br = '</div><br>';
+				var div4 = `<div class="col-4" style="min-width:250px;"><div class="column" style="text-decoration: underline;">`;
+				var divc = `<div class="column">`;
+				var div1br = `</div><br>`;
 
 				//Lak Only: Lak puts everyone in observer and doesnt update team rank
 				if(data.info_maptype == "LakRabbit"){
-					template += '<div class="col-6">';
+					template = `${template} <div class="col-6">`;
 					playerLoop(0, data);
 				}
 				//CTF
 				else{
 					//Team 1
-					template += '<div class="row-special">';
+					template = `${template} <div class="row-special">`;
 					playerLoop(1, data);
 					//Team 2
 					playerLoop(2, data);
@@ -111,7 +112,7 @@ $(document).ready
 				}
 			}
 			
-			$serverPopContainerList.html('<div style="font-size:45px;line-height: 55px;text-align: center;">' + template + div1);
+			$serverPopContainerList.html(`<div style="font-size:45px;line-height: 55px;text-align: center;"> ${template} ${div1}`);
 		}
 
 		getServerData(discordServerIP);
